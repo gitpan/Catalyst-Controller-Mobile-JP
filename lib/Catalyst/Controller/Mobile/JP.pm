b2a55c2da1cba0e5ff5c836fe4a3bcff69e9cdf6
Naoki Tomita <tomita@cpan.org>
2008-07-30 06:19:42 +0000

    Import of TOMITA/Catalyst-Controller-Mobile-JP-0.02 from CPAN.
    gitpan-cpan-distribution: Catalyst-Controller-Mobile-JP
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TOMITA/Catalyst-Controller-Mobile-JP-0.02.tar.gz
    gitpan-cpan-author:       TOMITA
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Controller/Mobile/JP.pm
+++ lib/Catalyst/Controller/Mobile/JP.pm
@@ -1,7 +1,7 @@
 package Catalyst::Controller::Mobile::JP;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use base qw( Catalyst::Controller Class::Accessor::Fast );
 __PACKAGE__->mk_accessors(qw( encoding ));
